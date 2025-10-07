b718530470b9c2f2f7052bcbbbb4a0a95343b1ef
Karan Singh <1king.karanbanna@gmail.com>
2025-10-07 11:11:46 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,76 @@
+# # method of list 
+# # append
+# list =["karan","bharat"]
+# list.append(5)
+# print(list)
+# # short      list ko badte karm m dalta h
+# print(list.sort())
+# print(list.sort(reverse=True))  #gat te karm m dalta h
+# print(list)
+
+# # reverse
+# list = [1,2,3,4,5,6,7,8,9]
+# list.reverse()
+# print(list)
+
+# # insert
+# list=[1,2,3,4,5]
+# list.insert(1,10)
+# print(list)
+
+# # remove  # remove  first occurrence of element
+# list=[1,2,1,3]
+# list.remove(1)
+# print(list)
+
+# # pop # index pe jaker element ko delete karna
+# list=[9,4,2]
+# list.pop(1)
+# print(list)
+
+# # tuple
+# tu=(1,)
+# print(tu)
+
+# # index
+# tu=(3,4,3,5,67,)
+# print(tu.index(67))
+# print(tu)
+
+# # count
+# print(tu.count(3))
+
+## prectice
+# #qustion 1
+# user1 = input("enter your first favrate movie : ")
+# user2 = input("enter your second favrate movie : ")
+# user3 = input("enter your third favrate movie : ")
+# user=[user1,user2,user3]
+# print(user)
+
+#or
+# user = []
+# user1 = input("enter your first favrate movie : ")
+# user2 = input("enter your second favrate movie : ")
+# user3 = input("enter your third favrate movie : ")
+# user.append(user1)
+# user.append(user2)
+# user.append(user3)
+# print(user)
+
+## qustion 2
+# list=[1,2,3,2,1]
+# list1=list.copy()
+# list1.reverse()
+# if(list==list1):
+#     print("list is palindrome")
+# else:
+#     print("this list is not palindrome")
+
+## qustion 3
+# tuple=("C","D","A","A","B","B","A")
+# print(tuple.count("A"))
+
+# list=["C","D","A","A","B","B","A"]
+# list.sort()
+# print(list)
